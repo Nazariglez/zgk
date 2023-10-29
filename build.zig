d49676ab8267a1f3fc4a59ef26f9878d3f774d66
Nazarí González <nazari.nz@gmail.com>
2023-10-29 18:22:02 +0000

    wip dawn

--- build.zig
+++ build.zig
@@ -5,7 +5,6 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = createLib(b, target, optimize);
-    //const lib = createExe(b, target, optimize);
 
     // examples
     addExamples(b, lib, target, optimize);
@@ -30,61 +29,38 @@ pub fn createLib(b: *std.Build, target: std.zig.CrossTarget, optimize: std.built
         .optimize = optimize,
     });
 
-    const lib_options = b.addOptions();
-    lib_options.addOption(bool, "empty", false);
-
-    lib.addOptions("build_options", lib_options);
-
-    // deps
-    addDependencies(b, lib, target, optimize);
-
     b.installArtifact(lib);
 
     return lib;
 }
 
-pub fn createExe(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) *std.Build.CompileStep {
-    const exe = b.addExecutable(.{
-        .name = "zgk",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(exe);
-
-    // deps
-    addDependencies(b, exe, target, optimize);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    return exe;
-}
-
-fn createModule(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) *std.Build.Module {
+fn linkDependencies(b: *std.Build, lib: *std.Build.CompileStep, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) *std.Build.Module {
     const zglfw = @import("deps/zig-gamedev/libs/zglfw/build.zig");
     const zglfw_pkg = zglfw.package(b, target, optimize, .{});
+    zglfw_pkg.link(lib);
+
+    const zpool = @import("deps/zig-gamedev/libs/zpool/build.zig");
+    const zpool_pkg = zpool.package(b, target, optimize, .{});
+    zpool_pkg.link(lib);
+
+    const zgpu = @import("deps/zig-gamedev/libs/zgpu/build.zig");
+    const zgpu_pkg = zgpu.package(b, target, optimize, .{
+        .deps = .{ .zpool = zpool_pkg.zpool, .zglfw = zglfw_pkg.zglfw },
+    });
+    zgpu_pkg.link(lib);
 
     return b.createModule(.{
         .source_file = .{ .path = thisDir() ++ "/src/main.zig" },
         .dependencies = &.{
             .{ .name = "zglfw", .module = zglfw_pkg.zglfw },
+            .{ .name = "zpool", .module = zpool_pkg.zpool },
+            .{ .name = "zgpu", .module = zgpu_pkg.zgpu },
         },
     });
 }
 
-fn addDependencies(b: *std.Build, lib: *std.Build.CompileStep, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) void {
-    const zglfw = @import("deps/zig-gamedev/libs/zglfw/build.zig");
-    const zglfw_pkg = zglfw.package(b, target, optimize, .{});
-    zglfw_pkg.link(lib);
-}
-
 fn addExamples(b: *std.Build, lib: *std.Build.CompileStep, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) void {
-        addExample(b,  "gfx_triangle", lib, target, optimize);
+    addExample(b, "gfx_triangle", lib, target, optimize);
 }
 
 // adapted from zig-gamedev
@@ -99,11 +75,9 @@ fn addExample(b: *std.Build, comptime name: []const u8, lib: *std.Build.CompileS
     exe.want_lto = false;
     exe.strip = optimize == .ReleaseFast;
 
-    exe.addModule("zgk", createModule(b, target, optimize));
+    const zgk_module = linkDependencies(b, lib, target, optimize);
+    exe.addModule("zgk", zgk_module);
     exe.linkLibrary(lib);
-
-    //_ = lib;
-    //exe.addModule("zgk", lib.module(b));
 
     const install_step = b.step(name, "Build '" ++ name ++ "' example");
     install_step.dependOn(&b.addInstallArtifact(exe, .{}).step);
